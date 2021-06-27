cbbdf3f9e8a00ee45a3842f54098639bf59ec5a1
Christopher Berner <christopherberner@gmail.com>
2021-06-27 11:13:51 -0700

    commit 2

--- src/storage/local/mod.rs
+++
@@ -1,7 +0,0 @@
-mod data_storage;
-mod file_storage;
-mod metadata_storage;
-mod response_helpers;
-
-pub use file_storage::FileStorage;
-pub use metadata_storage::ROOT_INODE;
--- src/storage/mod.rs
+++
@@ -1,9 +0,0 @@
-mod local;
-mod lock_table;
-mod message_handlers;
-mod raft_group_manager;
-mod raft_node;
-mod storage_node;
-
-pub use local::ROOT_INODE;
-pub use storage_node::Node;
